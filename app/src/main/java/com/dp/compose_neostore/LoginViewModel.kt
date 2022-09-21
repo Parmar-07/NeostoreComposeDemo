bd5d17f540ab0608dc6ef41eb204a0943240dd16
dinesh.parmar <dinesh.parmar@fawry.com>
2022-09-21 20:29:21 +0530

    added login fun inside login VM

--- app/src/main/java/com/dp/compose_neostore/LoginViewModel.kt
+++ app/src/main/java/com/dp/compose_neostore/LoginViewModel.kt
@@ -1,9 +1,25 @@
 package com.dp.compose_neostore
 
-import androidx.lifecycle.ViewModel
+import com.dp.compose_neostore.base.BaseViewModel
+import com.dp.nestore_domain.params.LoginParams
 import com.dp.nestore_domain.usecases.LoginUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class LoginViewModel @Inject constructor(val loginUseCase: LoginUseCase) : ViewModel()
+class LoginViewModel @Inject constructor(private val loginUseCase: LoginUseCase) : BaseViewModel() {
+
+
+    fun login(username: String, password: String) {
+        executeUseCaseResult({
+            val params = LoginParams(username, password)
+            loginUseCase.buildUseCase(params)
+        }, {
+
+        }, {
+
+        })
+    }
+
+
+}
---
+++ app/src/main/java/com/dp/compose_neostore/base/BaseViewModel.kt
@@ -0,0 +1,38 @@
+package com.dp.compose_neostore.base
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.dp.nestore_domain.base.ResultWrapper
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+open class BaseViewModel : ViewModel() {
+
+    private val bgJobs = ArrayList<Job>()
+
+    internal fun <T> executeUseCaseResult(task: suspend () -> ResultWrapper<Throwable,T>, onSuccess: (T) -> Unit,onError:(String)->Unit) {
+        val dfJob = viewModelScope.launch {
+            val result = withContext(this.coroutineContext) {
+                return@withContext withContext(this.coroutineContext) {
+                    task.invoke()
+                }
+            }
+            when(result){
+                is ResultWrapper.Success->onSuccess.invoke(result.value)
+                is ResultWrapper.Error -> onError.invoke(result.error.message?:"Something wrong!")
+            }
+         }
+        bgJobs.add(dfJob)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        bgJobs.forEach { job ->
+            if (!job.isCancelled) {
+                job.cancel()
+            }
+        }
+    }
+
+}
